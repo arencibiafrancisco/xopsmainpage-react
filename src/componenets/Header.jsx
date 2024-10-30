27bd9d75c1fae88d16e591c4cb42c815f1d32423
Francisco Arencibia Quesada <arencibia.francisco@gmail.com>
2024-10-30 22:29:55 +0100

    Update Header.jsx

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { BrowserRouter as Router } from 'react-router-dom';
 import logo from "../assets/xops.png";
 
 const Header = () => {
@@ -13,6 +12,9 @@ const Header = () => {
     };
 
     const scrollToPonentes = () => {
+
+        ponentes
+        
         const element = document.getElementById('ponentes');
         if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
@@ -20,33 +22,35 @@ const Header = () => {
     };
 
     const scrollToEvents = () => {
+
+        ponentes
+        
         const element = document.getElementById('events');
         if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
         }
     };
 
+
     return (
         <Router>
-            <Navbar bg="light" expand="lg" className="header">
-                <Navbar.Brand href="#home" className="text-white font-weight-bold">
-                    XOPS CONFERENCE
-                </Navbar.Brand>
-                <img src={logo} alt="Jane Doe" style={{ height: '51px', width: '56px' }} />
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-between">
-                    <Nav className="mx-auto">
-                        <Nav.Link onClick={scrollToEvents} className="links px-4 font-weight-bold text-white" href="#inicio">EVENTO</Nav.Link>
-                        <Nav.Link onClick={scrollToPonentes} className="links px-4 font-weight-bold text-white" href="#inicio">PONENTES</Nav.Link>
-                        <Nav.Link onClick={scrollToPatrocinio} className="links px-4 font-weight-bold text-white" href="#vuélvete-patrocinador">CONVIERTE PATROCINADOR</Nav.Link>
-                        <Nav.Link className="links px-4 font-weight-bold text-white" href="#tickets">ORGANIZADORES</Nav.Link>
-                    </Nav>
-                    <a href="https://www.eventbrite.es/e/entradas-xops-conference-1049115200807?aff=oddtdtcreator" className="button menu-btn">
-                        Tickets
-                    </a>
-                </Navbar.Collapse>
-            </Navbar>
-        </Router>
+        <Navbar bg="light" expand="lg" className='header'>
+        <Navbar.Brand href="#home" className='text-white font-weight-bold'>XOPS CONFERENCE</Navbar.Brand>
+        <img src={logo} alt="Jane Doe" style={{ height: '51px', width: '56px' }} />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-between">
+            <Nav className="mx-auto ">
+            <Nav.Link onClick={scrollToEvents} className='links px-4 font-weight-bold text-white' href="#inicio">EVENTO</Nav.Link>
+                <Nav.Link onClick={scrollToPonentes} className='links px-4 font-weight-bold text-white' href="#inicio">PONENTES</Nav.Link>
+                <Nav.Link onClick={scrollToPatrocinio} className='links px-4 font-weight-bold text-white ' href="#vuélvete-patrocinador">CONVIERTE PATROCINADOR</Nav.Link>
+                <Nav.Link className='links px-4 font-weight-bold text-white' href="#tickets"></Nav.Link>
+            </Nav>
+        <a href="https://www.eventbrite.es/e/entradas-xops-conference-1049115200807?aff=oddtdtcreator" class="button menu-btn">
+            Tickets
+        </a>
+        </Navbar.Collapse>
+    </Navbar>
+    </Router>
     );
 };
 
