e97730bc6e1bdfe33f2e9dbb1126261de578d6f5
Specter <spectertj@gmail.com>
2024-10-31 01:25:00 +0000

    vite
    Signed-off-by: Specter <spectertj@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/xops/',
-  build: {
-    outDir: 'xops',
-  },
   optimizeDeps: {
     include: [
       'react-bootstrap',  // Asegúrate de incluir react-bootstrap
