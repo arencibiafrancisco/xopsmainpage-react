4233663d7b6c48b495793e2d0176ad08a95f46a0
Specter <spectertj@gmail.com>
2024-11-06 23:49:25 +0000

    fix in the workflow
    Signed-off-by: Specter <spectertj@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/xops/',
   optimizeDeps: {
     include: [
       'react-bootstrap',  // Asegúrate de incluir react-bootstrap
