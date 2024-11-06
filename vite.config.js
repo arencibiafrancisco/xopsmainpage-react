9c67df1fc7de493b1b7bd7661ea0bf0f275b5eed
Specter <spectertj@gmail.com>
2024-11-06 23:58:31 +0000

    dsit folder
    Signed-off-by: Specter <spectertj@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/xopsmainpage-react/',
+  base: '/dist/',
   optimizeDeps: {
     include: [
       'react-bootstrap',  // Asegúrate de incluir react-bootstrap
