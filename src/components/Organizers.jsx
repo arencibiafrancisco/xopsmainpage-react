cefd13e26d054211f1368f4176726e2d7e3042cf
Francisco Arencibia Quesada <arencibia.francisco@gmail.com>
2024-11-07 09:56:27 +0100

    Update Organizers.jsx
    Signed-off-by: Francisco Arencibia Quesada <arencibia.francisco@gmail.com>

--- src/components/Organizers.jsx
+++ src/components/Organizers.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import robertoG from "../assets/organizers/robertoG.jpeg";
 import franciscoA from "../assets/organizers/franciscoA.jpg";
 import juanillaA from "../assets/organizers/juanillaA.jpg";
@@ -21,33 +20,27 @@ const Organizers = () => {
                     <strong>Posición:</strong> Perito y Analista Informático y Telefónico Forense.
                   </p>
                   <p>
-                  Investigador Digital. Docente. Productor. Redactor y Editor.
-                  <br/> 
-                  Co-Fundador y General Manager de Osintomático Conference y OsintomátiCOPS Conference. 
-                  <br/>
-                  Co-Fundador y CMO de XOps Conference. 
-                  <br/>
-                  Fundador, Coordinador General y miembro de Hack%27BuenosAires.
-                  <br/>
-                  Ponente en Congresos Internacionales como: Tapas&Hacks (Esp), DragonJar Con (Col), 
-                  <br/>
-                  HackForAll (Lat), IntelCon (Esp), Ekoparty (Arg), CICC2023 (Bol) y Osintomático Bootcamp (Esp). 
-                  <br/>
-                  Autodidacta. Eterno aprendiz. Osint e Ingeniería Social. 
-                  <br/>
-                  Cine y series freakys (Star Wars, GOT y todo eso).
+                    Investigador Digital. Docente. Productor. Redactor y Editor.
+                    <br /> 
+                    Co-Fundador y General Manager de Osintomático Conference y OsintomátiCOPS Conference. 
+                    <br />
+                    Co-Fundador y CMO de XOps Conference. 
+                    <br />
+                    Fundador, Coordinador General y miembro de Hack%27BuenosAires.
+                    <br />
+                    Ponente en Congresos Internacionales como: Tapas&Hacks (Esp), DragonJar Con (Col), 
+                    <br />
+                    HackForAll (Lat), IntelCon (Esp), Ekoparty (Arg), CICC2023 (Bol) y Osintomático Bootcamp (Esp). 
+                    <br />
+                    Autodidacta. Eterno aprendiz. Osint e Ingeniería Social. 
+                    <br />
+                    Cine y series freakys (Star Wars, GOT y todo eso).
                   </p>
-
                   <p>
-                    <strong>Redes:</strong><br/>
-                    <p>
-                    Twitter: <a href="https://twitter.com/robedugonzalez" target="_blank">@robedugonzalez</a><br/>
+                    <strong>Redes:</strong><br />
+                    Twitter: <a href="https://twitter.com/robedugonzalez" target="_blank">@robedugonzalez</a><br />
                     LinkedIn: <a href="https://www.linkedin.com/in/robertoegonzalez1" target="_blank">in/robertoegonzalez1</a>
-                    </p>
                   </p>
-
-                {/*  <button className="button mt-3">Aprende Más</button>  */}
-
                 </AnimationWrapper>
               </div>
               <div className="speaker-img">
@@ -60,7 +53,7 @@ const Organizers = () => {
             <div className="speaker1 d-flex justify-around margin-top">
               <div className="speaker-img">
                 <AnimationWrapper animation="fade-right" duration={1500}>
-                  <img src={franciscoA} alt="Jane Doe" style={{ height: '331px', width: '346px' }} />
+                  <img src={franciscoA} alt="Francisco Arencibia" style={{ height: '331px', width: '346px' }} />
                 </AnimationWrapper>
               </div>
               <div className="speaker-content">
@@ -69,19 +62,30 @@ const Organizers = () => {
                   <p>
                     <strong>Posición:</strong> DevSecOps
                   </p>
-
                   <p>
-                  Co-Fundador y COO de XOps Conference.
-                  <br/> 
-                  Organizador y miembro de HackMadrid%27.
-                  <br/>
-                  Chapter Leader DevSecCon Spain
-                  <br/>
-                  Autodidacta. Eterno aprendiz. 
-                  <br/>
+                    Ingeniero DevOps con más de 10 años de experiencia en tecnologías de información,
+                    <br />
+                    especializado en automatización, virtualización y nube. Actualmente trabaja en JOTELULU,
+                    <br />
+                    diseñando y manteniendo infraestructura en la nube con herramientas como CloudStack,
+                    <br />
+                    Docker, Kubernetes, Ansible, y Terraform. 
+                    <br />
+                    Co-Fundador y COO de XOps Conference.
+                    <br />
+                    Organizador y miembro de HackMadrid%27.
+                    <br />
+                    Chapter Leader de DevSecCon Spain.
+                    <br />
+                    Cibercooperante del Instituto Nacional de Ciberseguridad (INCIBE). 
+                    <br />
+                    Apasionado de la cultura hacker y el hacking ético.
+                    <br />
                   </p>
-
-                {/*  <button className="button mt-3">Aprende Más</button>  */}
+                  <p>
+                    <strong>Redes:</strong><br />
+                    LinkedIn: <a href="https://www.linkedin.com/in/franciscoarencibia/" target="_blank">in/franciscoarencibia</a>
+                  </p>
                 </AnimationWrapper>
               </div>
             </div>
@@ -94,34 +98,30 @@ const Organizers = () => {
                     <strong>Posición:</strong> DevSecOps
                   </p>
                   <p>
-                  Docente. Escritor. Investigador.
-                  <br/>
-                  Co-Fundador y CEO de XOps Conference.
-                  <br/> 
-                  Organizador y miembro de HackMadrid%27.
-                  <br/>
-                  Fundador, Coordinador General y miembro de HackBarcelona%27.
-                  <br/>
-                  Chapter Leader DevSecCon Spain
-                  <br/>
-                  Ponente en Congresos Internacionales como: BlackHatEU(UK), RootedCon (Esp), DragonJar Con (Col), Pacific Hackers Conference (USA), HackMiami Conference (USA), Ekoparty (Arg),  _CICC2023 (Bol) y WorldParty(ESP). 
-                  <br/>
-                  Autodidacta. Eterno aprendiz. 
+                    Docente. Escritor. Investigador.
+                    <br />
+                    Co-Fundador y CEO de XOps Conference.
+                    <br />
+                    Organizador y miembro de HackMadrid%27.
+                    <br />
+                    Fundador, Coordinador General y miembro de HackBarcelona%27.
+                    <br />
+                    Chapter Leader de DevSecCon Spain.
+                    <br />
+                    Ponente en Congresos Internacionales como: BlackHatEU(UK), RootedCon (Esp), DragonJar Con (Col), Pacific Hackers Conference (USA), HackMiami Conference (USA), Ekoparty (Arg), CICC2023 (Bol) y WorldParty(ESP). 
+                    <br />
+                    Autodidacta. Eterno aprendiz.
                   </p>
                   <p>
-                    <strong>Redes:</strong><br/>
-                    <p>
-                    Twitter: <a href="https://twitter.com/spectertj" target="_blank">@spectertj</a><br/>
+                    <strong>Redes:</strong><br />
+                    Twitter: <a href="https://twitter.com/spectertj" target="_blank">@spectertj</a><br />
                     LinkedIn: <a href="https://www.linkedin.com/in/spectertj" target="_blank">in/spectertj</a>
-                    </p>
                   </p>
-
-                {/*  <button className="button mt-3">Aprende Más</button>  */}
                 </AnimationWrapper>
               </div>
               <div className="speaker-img">
                 <AnimationWrapper animation="fade-left" duration={1500}>
-                  <img src={juanillaA} alt="Elif Christian" style={{ height: '331px', width: '346px' }}/>
+                  <img src={juanillaA} alt="Antonio Juanilla" style={{ height: '331px', width: '346px' }} />
                 </AnimationWrapper>
               </div>
             </div>
