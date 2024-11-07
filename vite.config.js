1f314782ad1ab109d4ce5e7298b02addfb69c96f
Specter <spectertj@gmail.com>
2024-11-07 00:31:22 +0000

    some fixes
    Signed-off-by: Specter <spectertj@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/xopsmainpage-react/',  // Asegúrate de reemplazar 'xopsmainpage-react' con el nombre correcto de tu repositorio
   plugins: [react()],
   optimizeDeps: {
     include: [
