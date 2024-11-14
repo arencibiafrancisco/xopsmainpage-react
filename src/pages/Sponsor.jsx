fa2df521ab4ff4d85c4d88bf531f61738a4dea21
jvrDvos <jvrDvos@gmail.com>
2024-11-14 08:33:45 -0500

    Updated Scrholl

--- src/pages/Sponsor.jsx
+++ src/pages/Sponsor.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PricingTable from "../components/PricingTable";
 import Benefits from "../components/Benefits";
+import { useLocation } from 'react-router-dom';
 
 
 const Sponsor = () => {
+
+  const location = useLocation(); 
+  useEffect(() => {
+      if (location.hash === '#patrocinio') {
+          const element = document.getElementById('patrocinio');
+          if (element) {
+              element.scrollIntoView({ behavior: 'smooth' });
+          }
+      }
+  }, [location]);
+
+
   return (
     <>
+    <div id="patrocinio">
     <Benefits/>
     <PricingTable />
+    </div>
     </>
   );
 };
